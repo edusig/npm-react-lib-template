f66a9eebed4bdae1bdc2b2b94a5500361226c674
Eduardo Ciciliato <eduardo.ciciliato@gmail.com>
2023-10-30 00:16:42 -0300

    fix: disables import/named for the example test

--- src/example.test.tsx
+++ src/example.test.tsx
@@ -1,4 +1,6 @@
 import '@testing-library/jest-dom';
+// FIXME: for some reason eslint is not finding screen in this package
+// eslint-disable-next-line import/named
 import { render, screen } from '@testing-library/react';
 import * as React from 'react';
 import { ExampleComponent } from './example';
